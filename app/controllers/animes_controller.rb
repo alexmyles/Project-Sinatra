3947900e88b4d78a45943c849b05a61b6e2d9808
Alex <alex16myles@gmail.com>
2021-01-10 21:30:20 -0600

    added anime description

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -4,4 +4,30 @@ class AnimesController < ApplicationController
         @animes = Anime.all
         erb :'animes/index'
     end
+   
+    get '/animes/new' do
+        erb :'animes/new'
+    end
+
+    post '/animes' do
+        anime = current_user.anime.build(params[:anime])
+
+        if anime.save
+            redirect '/animes'
+
+        else
+            redirect '/animes/new'
+        end
+    end
+
+    get '/animes/:id' do 
+        find_anime
+        erb :'animes/show'
+    end
+
+    private
+
+    def find_anime
+        @anime = Anime.find_by_id(params[:id])
+    end
 end 
